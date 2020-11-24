ae9d4ef99808a938abd8feb4c395c9bd0610ba64
alex <xiayao.zeng@gmail.com>
2020-11-24 22:26:42 +0800

    feat: add generator-export

---
+++ generator-export/README.md
@@ -0,0 +1,36 @@
+# generator-export [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> generate export declaration for javascript and typescript
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-export using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-export
+```
+
+Then generate your new project:
+
+```bash
+yo export
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [alex]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-export.svg
+[npm-url]: https://npmjs.org/package/generator-export
+[travis-image]: https://travis-ci.com/alexxyzeng/generator-export.svg?branch=master
+[travis-url]: https://travis-ci.com/alexxyzeng/generator-export
+[daviddm-image]: https://david-dm.org/alexxyzeng/generator-export.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/alexxyzeng/generator-export
---
+++ generator-export/generators/app/index.js
@@ -0,0 +1,60 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const process = require("process");
+const childProcess = require("child_process");
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the superb ${chalk.red("generator-export")} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: "confirm",
+        name: "isTS",
+        message: "是否使用TypeScript？",
+        default: false
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "请输入要生成导出的路径",
+        default: process.cwd()
+      },
+      {
+        type: "input",
+        name: "fileName",
+        message: "请输入要生成的文件名",
+        default: "index"
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    console.log(this.props, "---- props");
+    // This.fs.copy(
+    //   this.templatePath("dummyfile.txt"),
+    //   this.destinationPath("dummyfile.txt")
+    // );
+    const { isTS, path, fileName } = this.props;
+    const extension = isTS ? "ts" : "js";
+    childProcess.execSync(
+      `npx generate-export ${path} ${fileName}.${extension} ${isTS}`,
+      { stdio: "inherit" }
+    );
+  }
+
+  // Install() {
+  //   // this.installDependencies({ yarn: true });
+  //   console.log(this.props);
+  // }
+};
